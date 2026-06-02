5dc45e7dd3e284d01521808755b09f83fe74bb4d
HELLORÁ BUENO MIRANDA <hellora_miranda@estudante.sesisenai.org.br>
2026-06-02 16:14:03 -0300

    instanciar personagem

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
-#include "personagem.h"
+#include "Personagem.h"
 
 int main () {
 	
-	
-	
-	
+	Personagem heroi("heroi", 100, 10);
+	heroi.apresentar();
 	return 0;
+	
+	
 }
